4116df32335d42d37386599f82e05204f3bda176
Valeriu Blascu <20084496@mail.wit.ie>
2021-01-09 17:30:33 +0000

    Topic 11 - Auth

--- reactApp/src/sample/authHeader.js
+++ reactApp/src/sample/authHeader.js
@@ -7,8 +7,8 @@ const BaseAuthHeader = (props) => {
   const { history } = props;
 
   return context.isAuthenticated ? (
-    <p>
-      Welcome! <button onClick={() => context.signout()}>Sign out</button>
+<p>
+      Welcome {context.userName}! <button onClick={() => context.signout()}>Sign out</button>
     </p>
   ) : (
     <p>
